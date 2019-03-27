cc3dad82ecca6d312d8de72ab6c0e7d0d5eefca9
WillyD <sopwill3@gmail.com>
2019-03-26 22:02:46 -0600

    Finished first draft of the pokemon game...its dirty and ugly but it works.

--- js/app.js
+++ js/app.js
@@ -1,79 +1,5 @@
 // POKEMON HOMEWORK
 
-const cards = [{
-  name: "Venusaur",
-  damage: 100,
-  attack: "Razor Leaf",
-}, {
-  name: "Charizard",
-  damage: 100,
-  attack: "Flamethrower",
-}, {
-  name: "Blastoise",
-  damage: 100,
-  attack: "Hydro-Pump",
-}, {
-  name: "Pidgeot",
-  damage: 85,
-  attack: "Steel Wing",
-}, {
-  name: "Golem",
-  damage: 90,
-  attack: "Earthquake",
-}, {
-  name: "Primeape",
-  damage: 75,
-  attack: "Dynamic Punch",
-}, {
-  name: "Persian",
-  damage: 70,
-  attack: "Payday",
-}, {
-  name: "Nidoqueen",
-  damage: 80,
-  attack: "Mega-Kick",
-}, {
-  name: "Nidoking",
-  damage: 85,
-  attack: "Horn-Drill",
-}, {
-  name: "Bellossom",
-  damage: 65,
-  attack: "Poisonpowder",
-}, {
-  name: "Magikarp",
-  damage: 0,
-  attack: "Splash",
-}, {
-  name: "Raichu",
-  damage: 80,
-  attack: "Thunderbolt",
-}, {
-  name: "Poliwrath",
-  damage: 75,
-  attack: "Surf",
-}, {
-  name: "Gyrados",
-  damage: 95,
-  attack: "Dragon-Rage",
-}, {
-  name: "Machamp",
-  damage: 70,
-  attack: "Strength",
-}, {
-  name: "Ninetails",
-  damage: 85,
-  attack: "Fire-Blast",
-}, {
-  name: "Alakazam",
-  damage: 95,
-  attack: "Psychic",
-}, {
-  name: "Mewtwo",
-  damage: 150,
-  attack: "Hyper Beam",
-}]
-// 18 cards
 
 /* PSUEDO
 
@@ -179,3 +105,241 @@ PROCESS 5 (finishing round)
     -contains the current round
 
 */
+
+
+
+// DEFINING OBJECTS
+
+// cards to be played
+const cards = [{
+  name: "Venusaur",
+  attack: "Razor Leaf",
+  damage: 100,
+}, {
+  name: "Charizard",
+  attack: "Flamethrower",
+  damage: 100,
+}, {
+  name: "Blastoise",
+  attack: "Hydro-Pump",
+  damage: 100,
+}, {
+  name: "Pidgeot",
+  attack: "Steel Wing",
+  damage: 85,
+}, {
+  name: "Golem",
+  attack: "Earthquake",
+  damage: 90,
+}, {
+  name: "Primeape",
+  attack: "Dynamic Punch",
+  damage: 75,
+}, {
+  name: "Persian",
+  attack: "Payday",
+  damage: 70,
+}, {
+  name: "Nidoqueen",
+  attack: "Mega-Kick",
+  damage: 80,
+}, {
+  name: "Nidoking",
+  attack: "Horn-Drill",
+  damage: 85,
+}, {
+  name: "Bellossom",
+  attack: "Poisonpowder",
+  damage: 65,
+}, {
+  name: "Magikarp",
+  attack: "Splash",
+  damage: 0,
+}, {
+  name: "Raichu",
+  attack: "Thunderbolt",
+  damage: 80,
+}, {
+  name: "Poliwrath",
+  attack: "Surf",
+  damage: 75,
+}, {
+  name: "Gyarados",
+  attack: "Dragon-Rage",
+  damage: 95,
+}, {
+  name: "Machamp",
+  attack: "Strength",
+  damage: 70,
+}, {
+  name: "Ninetails",
+  attack: "Fire-Blast",
+  damage: 85,
+}, {
+  name: "Alakazam",
+  attack: "Psychic",
+  damage: 95,
+}, {
+  name: "Mewtwo",
+  attack: "Hyper Beam",
+  damage: 150,
+}]
+//18 cards
+
+// discarded cards
+const discarded = [];
+
+// cards in play
+const battleField = [];
+
+// player
+const player = {
+  name: "changethis",//prompt("Please enter your name"),
+  hand: [],
+  roundsWon: 0,
+}
+
+// rounds
+let rounds = 0;
+
+// computer
+const computer = {
+  name: "changethis2",//prompt("Please enter your rival's name"),
+  hand: [],
+  roundsWon: 0,
+}
+
+var randomNumber = Math.floor(Math.random() * 2);
+
+const createAttackMultiplier = () => {return (Math.floor(Math.random() * 20) - 5)}
+
+
+const randomCard = (user) => {
+    let randomCard = Math.floor(Math.random() * cards.length)
+    user.hand.push(cards[randomCard]);
+    console.log(`${cards[randomCard].name} was drawn!`);
+    cards.splice(randomCard,1);
+}
+
+
+
+const drawCard = () => {
+
+    setTimeout(function(){
+      console.log(`${player.name} drew a card!`)
+    randomCard(player);
+  }, 500)
+
+    setTimeout(function(){
+      console.log(`${computer.name} drew a card!`)
+    randomCard(computer);
+  }, 1000)
+
+setTimeout(function(){
+  if (computer.hand.length === 3) {
+      console.log('You have chosen 3 Pokémon! Type "seeHand()" to see their information!');
+    } else {
+      console.log("Please draw another card!");
+    }
+  }, 1500)
+
+    return '""""""""""""""""'
+}
+
+
+
+const seeHand = () => {
+  console.log(player.hand);
+  console.log("Choose which Pokémon to send into battle! The higher the damage the better the Pokémon!");
+  console.log(`Type "iChooseYou('Name-Of-Pokemon')" to make your choice`);
+  return '"""""""""""""""""';
+}
+
+
+
+const iChooseYou = (name) => {
+  for (i = 0; i < 3; i++){
+    if (player.hand[0]['name'] === name) {
+        battleField.push(player.hand[0]);
+        player.hand.splice(0,1);
+    } else {
+        discarded.push(player.hand[0]);
+        player.hand.splice(0,1);
+    }
+  }
+  console.log(`I choose you! ${name}!`);
+
+  setTimeout(function(){computerChoosePokemon(randomNumber);},2000);
+
+  return '"""""""""""""""'
+}
+
+const computerChoosePokemon = (num) => {
+  battleField.push(computer.hand[num]);
+  console.log(`${computer.name} sent ${computer.hand[num].name} into battle!`);
+  computer.hand.splice(num,1);
+  for (i = 0; i < 2; i++){
+  discarded.push(computer.hand[i]);
+  computer.hand.splice(i,1);
+  }
+  console.log(`Type "attack()" to have ${battleField[0].name} attack!`);
+}
+
+
+const attack = () => {
+
+  let playerDamage = battleField[0].damage + createAttackMultiplier();
+  let computerDamage = battleField[1].damage + createAttackMultiplier();
+
+  console.log(`${battleField[0].name} used ${battleField[0].attack} for ${playerDamage} damage!`);
+
+  setTimeout(function(){console.log(`${battleField[1].name} used ${battleField[1].attack} for ${computerDamage} damage!`);},2000)
+
+  setTimeout(function(){
+    if (playerDamage > computerDamage) {
+      console.log(`${battleField[0].name} wins! ${battleField[1].name} fainted! Great job!`);
+      player.roundsWon++;
+      rounds++
+      if (rounds === 3 && player.roundsWon > computer.roundsWon) {
+        alert(`Congratulations! You win! Refresh the page to try again!`)
+      } else if (rounds === 3 && player.roundsWon < computer.roundsWon) {
+        alert(`I'm Sorry! You lose! Refresh the page to try again!`)
+      } else if (rounds === 3) {
+        alert("Tie game! Refresh the page to try again!")
+      }
+  } else if (playerDamage === computerDamage) {
+      console.log(`The attack failed...${battleField[0].name} did nothing...`);
+      rounds++
+      if (rounds === 3 && player.roundsWon > computer.roundsWon) {
+        alert(`Congratulations! You win! Refresh the page to try again!`)
+      } else if (rounds === 3 && player.roundsWon < computer.roundsWon) {
+        alert(`I'm Sorry! You lose! Refresh the page to try again!`)
+      } else if (rounds === 3) {
+        alert("Tie game! Refresh the page to try again!")
+      }
+  } else {
+    console.log(`${battleField[1].name} wins! ${battleField[0].name} fainted!`);
+    computer.roundsWon++;
+    rounds++
+
+  }},4000)
+  setTimeout(function(){discarded.push(battleField[0]);
+  discarded.push(battleField[1]);
+  battleField.splice(0,2);
+  console.log('Type "drawCard()" to draw another card!');},6000)
+
+  return '""""""""""'
+}
+
+
+// THIS IS THE FLOW OF THE ACTUAL GAME THAT PROMPTS USERS TO PLAY WITHOUT TYPED FUNCTIONS
+
+console.log(`${computer.name} would like to battle!`);
+console.log('Please type "acceptBattle()" to begin!');
+console.log('""""""""""""""""');
+
+const acceptBattle = function() {
+  console.log(`${player.name} accepted the challenge! Let the battle begin!`);
+  console.log('Please type "drawCard()" to choose your Pokémon!');
+  return '""""""""""""""""'
+}
