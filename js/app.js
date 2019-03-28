696486dcc3b5bf7041c4369c029c5acab42a5e0f
Sami Austin <samicaustin@gmail.com>
2019-03-27 18:29:35 -0600

    Messy first attempt--need to try again with an OOP focus

--- js/app.js
+++ js/app.js
@@ -2,81 +2,192 @@ console.log("Pokemon Game");
 //Pokemon cards--array of objects!
 
 // 1. keep a library of all the Pokemon cards that can be played (see the array in the "The Cards" section)
-const game = {
-    deck: [
-        {
-          name: "Bulbasaur",
-          damage: 60
-        }, {
-          name: "Caterpie",
-          damage: 40
-        }, {
-          name: "Charmander",
-          damage: 60
-        }, {
-          name: "Clefairy",
-          damage: 50
-        }, {
-          name: "Jigglypuff",
-          damage: 60
-        }, {
-          name: "Mankey",
-          damage: 30
-        }, {
-          name: "Meowth",
-          damage: 60
-        }, {
-          name: "Nidoran - female",
-          damage: 60
-        }, {
-          name: "Nidoran - male",
-          damage: 50
-        }, {
-          name: "Oddish",
-          damage: 40
-        }, {
-          name: "Pidgey",
-          damage: 50
-        }, {
-          name: "Pikachu",
-          damage: 50
-        }, {
-          name: "Poliwag",
-          damage: 50
-        }, {
-          name: "Psyduck",
-          damage: 60
-        }, {
-          name: "Rattata",
-          damage: 30
-        }, {
-          name: "Squirtle",
-          damage: 60
-        }, {
-          name: "Vulpix",
-          damage: 50
-        }, {
-          name: "Weedle", 
-          damage: 40
-        }
-      ],
-      cardsPlayed: [],
-      cardsLeft: 18,
-      round: 1,
-      wins: {
-          player: 0,
-          computer: 0
-      },
-      dealThree(){
+deck = [
+  {
+    name: "Bulbasaur",
+    damage: 60
+  }, {
+    name: "Caterpie",
+    damage: 40
+  }, {
+    name: "Charmander",
+    damage: 60
+  }, {
+    name: "Clefairy",
+    damage: 50
+  }, {
+    name: "Jigglypuff",
+    damage: 60
+  }, {
+    name: "Mankey",
+    damage: 30
+  }, {
+    name: "Meowth",
+    damage: 60
+  }, {
+    name: "Nidoran - female",
+    damage: 60
+  }, {
+    name: "Nidoran - male",
+    damage: 50
+  }, {
+    name: "Oddish",
+    damage: 40
+  }, {
+    name: "Pidgey",
+    damage: 50
+  }, {
+    name: "Pikachu",
+    damage: 50
+  }, {
+    name: "Poliwag",
+    damage: 50
+  }, {
+    name: "Psyduck",
+    damage: 60
+  }, {
+    name: "Rattata",
+    damage: 30
+  }, {
+    name: "Squirtle",
+    damage: 60
+  }, {
+    name: "Vulpix",
+    damage: 50
+  }, {
+    name: "Weedle", 
+    damage: 40
+  }
+];
 
-      },
-      whoWins(){
+const computer = {
+  cardPlayed: [],
+  wins: 0
+};
 
-      },
+const player = {
+  cardPlayed: [],
+  wins: 0
+};
+
+let computerCardPlayed = "";
+
+// This function is choosing three randomized cards from the deck,
+// then selecting one card at random at a time until the currentHand 
+// is zero. 
+
+const computerChooses = () => {
+  const currentHand = [];
+  let currentCard = "";
+
+  while (currentHand.length <= 2) {
+    let randomIndex = Math.floor(Math.random()* deck.length);
+    currentCard = deck[randomIndex];
+
+    // puts current card into hand to make a hand of three
+    currentHand.push(currentCard);
+    console.log(currentHand);
+    //removes pulled cards from deck
+    deck.splice(randomIndex, 1);
+    console.log(deck);
+  };
+
+  for (let i = 0; i < 3; i++) {
+    let randomIndex3 = Math.floor(Math.random()* currentHand.length);
+    computerCardPlayed = currentHand[randomIndex3];
+    console.log(`The computer chose ${computerCardPlayed.name}!`);
+    computer.cardPlayed.push(computerCardPlayed);
+    currentHand.splice(randomIndex3, 1);
+    return playerCardPlayed;
+  };
+};
+
+
+let playerCardPlayed = "";
+const playerHand = [];
+
+playerChooses = () => {
+  let currentCard = "";
+  
+  while (playerHand.length <= 2) {
+    let randomIndex = Math.floor(Math.random()* deck.length);
+    currentCard = deck[randomIndex];
+
+    // puts current card into hand to make a hand of three
+    playerHand.push(currentCard);
+
+    deck.splice(randomIndex, 1);
+  };
+  
+  for (let i = 0; i < 3; i++) {
+    let randomIndex3 = Math.floor(Math.random()* playerHand.length);
+    playerCardPlayed = playerHand[randomIndex3];
+    console.log(`You chose ${playerCardPlayed.name}!`);
+    player.cardPlayed.push(playerCardPlayed);
+
+    playerHand.splice(randomIndex3, 1);
+    return playerCardPlayed;
+  };
+};
+
+
+
+battle = () => {
+
+  for (i = 0; i < playerHand.length; i++){
+    if (playerCardPlayed.damage === computerCardPlayed.damage) {
+      console.log(`Tie game! No one earns any points.`);
+    } else if (playerCardPlayed.damage > computerCardPlayed.damage) {
+      console.log(`Your ${playerCardPlayed.name} knocked out the 
+      computer's ${computerCardPlayed.name}!`);
+      player.wins++;
+      console.log(`You have won ${player.wins} games! `);
+    } else {
+      console.log(`The computer's ${computerCardPlayed.name} knocked out 
+      your ${playerCardPlayed.name}!`);
+      computer.wins++;
+      console.log(`The computer has won ${computer.wins} games!`);
+    };
+  };
+  
+};
+
+
+
+playerChooses();
+computerChooses();
+battle();
+
+console.log(player);
+console.log(computer);
+
+
+
+
+
+startRound = () => {
+  playerChooses();
+  computerChooses();
+  battle();
 
 };
 
-console.log(game.deck[0].name); // ==> Bulbasaur
+
+
+
+
+  // // use that number as an index in the deck array
+  // const computerPlayed = deck[randomIndex];
+  // // set the card played propery in the computer object
+  // computer.cardPlayed = computerPlayed;
+
+
+// cardsPlayed: [];
+// cardsLeft: 18;
+// round: 1;
+
+
+
 
 // 2. know what cards have been played
     // game.cardsPlayed is an empty array--will need to push card objects into it during gameplay
@@ -96,7 +207,8 @@ console.log(game.deck[0].name); // ==> Bulbasaur
     // game.wins.player/computer holds values of zero 
 
 // 7. automatically deal 3 cards from the library to the player and 3 cards to the computer each round
-    // dealThree is a function that will provide three randomized cards as options for both players.
+    // dealThree is a function that will provide three randomized cards to each player. 
+            //var item = items[Math.floor(Math.random()*items.length)];
     // once each played has played all three of their cards, currentRound needs to increase by one.
 
 // 8. determine the winner of each play
@@ -104,19 +216,19 @@ console.log(game.deck[0].name); // ==> Bulbasaur
     // highest earns one point in game.wins.player/computer
 
 // 9. stop once there are no cards left or not enough to deal 3 to each the player and computer
-    // function that maybe needs to be outside of the the object? 
 
-const player = {
-    stats: {
-        points: 0,
-        roundsWon: 0
-    },
-    currentHand: [],
-    playCard(){
 
-    },
-    cardsPlayed: []
-};
+// const player = {
+//     stats: {
+//         points: 0,
+//         roundsWon: 0
+//     },
+//     currentHand: [],
+//     playCard(){
+
+//     },
+//     cardsPlayed: []
+// };
 
 // 1. see their stats: their points and how many rounds they've won.
 
@@ -126,11 +238,21 @@ const player = {
 
 // 3. pick a card from the hand that has been dealt to them (thereby playing this 
     // card agaist the computer's card). The round ends once this has happened 3 times.
-    // FUNCTION that prompts the user to choose one of three cards. 
+    // FUNCTION that chooses and plays one of three cards. 
 
 // 4. receive new cards given to them by the game each round.
     // dealThree will repopulate the cleared currentHand
 
 // 5. see the cards that they have played in the past.
     // each card played will get pushed into player.cardsPlayed
+
+
+
+// Start by creating all the neccessary variables and functions BEFORE writing
+// any code. Pseudocode into each function too before coding. 
+
+// Make small functions first! Write it before you define it. 
+
+
+
 
